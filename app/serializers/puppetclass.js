331f35d36a7214d884a90f16f36dd1c20634c569
Joseph Magen <joseph@isratrade.co.il>
2015-01-27 17:33:00 +0200

    inherit PuppetSerializer from AppplciationSerializer

--- app/serializers/puppetclass.js
+++ app/serializers/puppetclass.js
@@ -1,12 +1,6 @@
-import DS from 'ember-data';
+import ApplicationSerializer from './application';
 
-export default DS.ActiveModelSerializer.extend({
-  extractArray: function(store, type, payload) {
-    var wrapped_payload = {};
-    var model_name = type.toString().split(":")[1];
-    wrapped_payload[model_name] = $.map(payload['results'], function(v) { return v;  });
-    return this._super(store, type, wrapped_payload);
-  },
+export default ApplicationSerializer.extend({
 
   attrs: {
     module_name2: {key: 'module_name'},
