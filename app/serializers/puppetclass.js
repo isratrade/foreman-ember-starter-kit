7074cc57b314d890483bab169080bb0272c7219e
Joseph Magen <joseph@isratrade.co.il>
2015-01-27 17:11:19 +0200

    removed module_name as DS.attr since it's reserve method name in ember

--- app/serializers/puppetclass.js
+++ app/serializers/puppetclass.js
@@ -7,28 +7,6 @@ export default DS.ActiveModelSerializer.extend({
     var model_name = type.toString().split(":")[1];
     wrapped_payload[model_name] = $.map(payload['results'], function(v) { return v;  });
     return this._super(store, type, wrapped_payload);
-  },
+  }
 
-  attrs: {
-    // module_group: 'module_name'
-    module_name2: {key: 'module_name'},
-    name2: {key: 'name'}
-  },
-
-  // normalizeHash: {
-  //   module_group: function(hash) {
-  //     hash.module_name = hash.module_group;
-  //     delete hash.module_group;
-
-  //     return hash;
-  // }
-
-  // keyForAttribute: function(attr) {
-  //   //var newAttr = ( (attr === 'module_name') ? 'module_group' : attr)
-  //   var newAttr = attr;
-  //   if (attr === 'module_group') {
-  //     newAttr = 'module_name';
-  //   }
-  //   return newAttr;
-  // }
 });
