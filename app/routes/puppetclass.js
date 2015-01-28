1d69ccde17aaa1adf082e77c8e8fb66759882342
Joseph Magen <joseph@isratrade.co.il>
2015-01-28 15:10:42 +0200

    smart class parameters per puppetclass

--- app/routes/puppetclass.js
+++ app/routes/puppetclass.js
@@ -8,9 +8,9 @@ export default Ember.Route.extend({
   actions: {
     saveRecord: function() {
       var self = this;
-      console.log(this);
-        console.log('currentModel');
-        console.log(this.get('currentModel'));
+      // console.log(this);
+      //   console.log('currentModel');
+      //   console.log(this.get('currentModel'));
       this.get('currentModel').save().then(function() {
         self.transitionTo('puppetclasses');
       }, function(response) {
--- app/routes/puppetclass/smart-class-parameters.js
+++ app/routes/puppetclass/smart-class-parameters.js
@@ -2,9 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model:function(params) {
-    console.log('PARAMS');
-    var a = this.modelFor('puppetclass')
-    console.log(a.get('name'));
-    return this.store.find('smart-class-parameter', {puppetclass_id: params.puppetclass_id});
+    var puppetclass = this.modelFor('puppetclass')
+    return this.store.find('smart-class-parameter', {puppetclass_id: puppetclass.id});
   }
 });
