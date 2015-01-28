059e46172f9ad230188335906cd428e7b7db4192
Joseph Magen <joseph@isratrade.co.il>
2015-01-28 22:30:58 +0200

    hostgroup stuff

---
+++ app/serializers/hostgroup.js
@@ -0,0 +1,16 @@
+import DS from 'ember-data';
+
+export default DS.RESTSerializer.extend({
+
+  // attrs: {
+  //   operatingsystem_name: {serialize: false},
+  //   subnet_name: {serialize: false},
+  //   domain_name: {serialize: false},
+  //   environment_name: {serialize: false},
+  //   compute_profile_name: {serialize: false},
+  //   architecture_name: {serialize: false},
+  //   medium_name: {serialize: false},
+  //   ptable_name: {serialize: false},
+  // }
+
+});
