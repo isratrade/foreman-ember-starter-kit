01349b6125de22eba57cee66dfac3265c0f92bcc
Joseph Magen <joseph@isratrade.co.il>
2015-01-30 19:26:03 +0200

    hostgroups filter

--- app/routes/hostgroups.js
+++ app/routes/hostgroups.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('hostgroup');
-  }
+  },
+  setupController: function(controller, model) {
+    controller.set('model', model);
+  },
+
 });
